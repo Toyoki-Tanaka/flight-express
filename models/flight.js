9cc77c5ad9df34b45bad7911fdb7567fda5b5926
SamteaSilver <tanakatoyoki19@gmail.com>
2023-09-12 17:16:44 -0500

    Added build script

--- models/flight.js
+++ models/flight.js
@@ -1,10 +1,12 @@
 const { Schema, model } = require('mongoose');
-
+const destinationSchema = require('./destination')
 const flightSchema = new Schema(
     {
         airline: { type: String, enum: ['American', 'Southwest', 'United'] },
         flightNo: { type: Number, required: true, min: 10, max: 9999 },
-        departs: { type: Date, default: () => new Date(+new Date() + 365 * 24 * 60 * 60 * 1000) }
+        departs: { type: Date, default: () => new Date(+new Date() + 365 * 24 * 60 * 60 * 1000) },
+        airport: { type: String, enum: ['AUS', 'DAL', 'LAX', 'SAN', 'SEA'], default: 'SAN' },
+        destinations: [destinationSchema]
     },
     // Add Default date an year from now
     {
